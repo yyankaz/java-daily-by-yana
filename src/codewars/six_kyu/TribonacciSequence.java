d88c7fbf34a28f82ce966393e87d8de50b8a6cff
Zavalniuk Yana <yana.zavalniuk@stud.onu.edu.ua>
2025-06-29 14:49:40 +0200

    new task added

---
+++ src/codewars/six_kyu/TribonacciSequence.java
@@ -0,0 +1,48 @@
+package codewars.six_kyu;
+
+//Well met with Fibonacci bigger brother, AKA Tribonacci.
+//
+//As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of
+// the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers
+// trying to pronounce it :(
+//
+//So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+//
+//[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+//
+//But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the
+// common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by
+// 2 places, but that is not the case and we would get:
+//
+//[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+//
+//Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature
+// array/list, returns the first n elements - signature included of the so seeded sequence.
+//
+//Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty
+// array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TribonacciSequence {
+    public double[] tribonacci(double[] s, int n) {
+        if(n == 0){
+            double[] empty = {};
+            return empty;}
+        List<Double> list = new ArrayList<>();
+        for (int i = 0; i < Math.min(s.length, n); i++) {
+            list.add(s[i]);
+        }
+        for (int i = list.size(); i < n; i++) {
+            list.add(list.get(i - 3) + list.get(i - 2) + list.get(i - 1));
+        }
+
+        double[] result = new double[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
